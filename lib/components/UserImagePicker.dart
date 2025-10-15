8b25308a5e17ce40734f5bfb4d7df715b9b656d8
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-15 08:58:12 -0300

    feat: add _retrieveLostData

--- lib/components/UserImagePicker.dart
+++ lib/components/UserImagePicker.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -14,6 +15,32 @@ class UserImagePicker extends StatefulWidget {
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _image;
 
+  // 1.  MÉTODO PARA RECUPERAR DADOS PERDIDOS
+  Future<void> _retrieveLostData() async {
+    final ImagePicker picker = ImagePicker();
+    // Faz a verificação se há dados perdidos
+    final LostDataResponse response = await picker.retrieveLostData();
+    // Se não há nada a recuperar, apenas retorna
+    if (response.isEmpty) {
+      return;
+    }
+    // Se há arquivos recuperados
+    final List<XFile>? files = response.files;
+    if (files != null && files.isEmpty) {
+      // Pega o primeiro arquivo (este widget só lida com uma imagem)
+      final XFile pickedFile = files.first;
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+      // Envia a imagem recuperada para o widget pai
+      widget.onImagePick(_image!);
+    } else {
+      if (kDebugMode) {
+        print(response.exception);
+      }
+    }
+  }
+
   Future<void> _pickerImage() async {
     final picker = ImagePicker();
     final pickedImage = await picker.pickImage(
@@ -22,13 +49,24 @@ class _UserImagePickerState extends State<UserImagePicker> {
       maxWidth: 150,
     );
 
-    if(pickedImage != null) {
+    if (pickedImage != null) {
+      // Cria o arquivo a partir da imagem escolhida
+      final newImage = File(pickedImage.path);
       setState(() {
-        _image = File(pickedImage.path);
+        _image = newImage;
       });
+      // Isso evita o erro de nulo caso o usuário cancele a seleção
+      widget.onImagePick(newImage);
     }
+  }
 
-    widget.onImagePick(_image!);
+  @override
+  void initState() {
+    super.initState();
+    // 2. CHAMAMOS A VERIFICAÇÃO QUANDO O WIDGET É INICIADO
+    // Isso garante que, se o app foi fechado pelo SO, recuperamos a imagem
+    // Usamos um Future.microtask para garantir que seja executado após a primeira build
+    Future.microtask(() => _retrieveLostData());
   }
 
   @override
