f0132a33708fe6a3f5a331fe1abc23ea8d39ecc8
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-17 07:51:41 -0300

    feat: add authService interface e mock implementation

---
+++ lib/core/services/auth_mock_service.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:chat_flutter/core/models/chat_user.dart';
+import 'package:chat_flutter/core/services/auth_services.dart';
+
+class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
+  @override
+  ChatUser? get currentUser => _currentUser;
+
+  @override
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  @override
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  @override
+  Future<void> signup(
+    String name,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image?.path ?? '/assest/image/...',
+    );
+
+    _users.putIfAbsent(email, () => newUser);
+
+    _updateUser(newUser);
+  }
+
+  @override
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
+}
