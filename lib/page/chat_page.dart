c315ddb9f6edeb50b944913b8158ceac281f5244
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-20 06:55:09 -0300

    feat: add logout in appBar

--- lib/page/chat_page.dart
+++ lib/page/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_flutter/components/messages.dart';
+import 'package:chat_flutter/components/new_message.dart';
 import 'package:chat_flutter/core/services/auth_services.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +9,56 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: Text('Cod3r Chat', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 10),
+                      Text('Sair'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              if( value == 'logout') {
+                AuthService().logout();
+              }
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Chat page'),
-            TextButton(onPressed: AuthService().logout, child: Text('Sair'))
+            Expanded(child: Message()),
+            NewMessages(),
           ],
         ),
       ),
     );
   }
 }
+
+// Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text('Chat page'),
+//             TextButton(onPressed: AuthService().logout, child: Text('Sair')),
+//           ],
+//         ),
+//       ),
