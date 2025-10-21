b84d19b4376e229997acddf0fe1c3cbdf3136f59
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-21 07:36:13 -0300

    feat: implement component message

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,8 @@
+import 'package:chat_flutter/components/message_bubble.dart';
+import 'package:chat_flutter/core/models/chat_message.dart';
+import 'package:chat_flutter/core/services/auth_services.dart';
+import 'package:chat_flutter/core/services/chat_service.dart';
+import 'package:chat_flutter/page/loading_page.dart';
 import 'package:flutter/material.dart';
 
 class Message extends StatelessWidget {
@@ -5,6 +10,29 @@ class Message extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(decoration: BoxDecoration(color: Colors.amber), child: Text('Mensagens')));
+    final currentUser = AuthService().currentUser;
+
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (ctx, snapShot) {
+        if (snapShot.connectionState == ConnectionState.waiting) {
+          return LoadingPage();
+        } else if (!snapShot.hasData || snapShot.data!.isEmpty) {
+          return Center(child: Text('Sem mensagens. Vamos conversar?'));
+        } else {
+          final msgs = snapShot.data!;
+
+          return ListView.builder(
+            itemCount: msgs.length,
+            itemBuilder: (ctx, i) => MessageBubble(
+              key: ValueKey(msgs[i].id),
+              message: msgs[i],
+              belongsToCurrentUser: currentUser?.id == msgs[i].userId,
+            ),
+            reverse: true,
+          );
+        }
+      },
+    );
   }
 }
