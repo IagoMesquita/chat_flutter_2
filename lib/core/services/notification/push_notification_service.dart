e75e0d7ada5cfbeafdc031d81488140ae11fd814
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-22 14:46:39 -0300

    class PushNotificationService with ChangeNotifier { feat: add

---
+++ lib/core/services/notification/push_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:chat_flutter/core/models/chat_notification.dart';
+import 'package:flutter/material.dart';
+
+class PushNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notificaion) {
+    _items.add(notificaion);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+    notifyListeners();
+  }
+}
