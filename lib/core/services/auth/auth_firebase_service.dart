b94ac4a4c318d3df2078c746e4823f270af37a93
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-10-23 16:10:05 -0300

    feat: add and use impl AuthFirebaseService

---
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:chat_flutter/core/models/chat_user.dart';
+import 'package:chat_flutter/core/services/auth/auth_services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthFirebaseService implements AuthService {
+  final auth = FirebaseAuth.instance;
+
+  static ChatUser? _currentUser;
+
+  static final _userStream = Stream<ChatUser?>.multi((controller) async {
+    final authChanges = FirebaseAuth.instance.authStateChanges();
+
+    await for (final user in authChanges) {
+      _currentUser = user == null ? null : _toChatUser(user);
+      controller.add(_currentUser);
+    }
+  });
+
+  @override
+  ChatUser? get currentUser => _currentUser;
+
+  @override
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  @override
+  Future<void> login(String email, String password) async {}
+
+  @override
+  Future<void> signup(
+    String name,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    try {
+      // Cria user com email e senha
+      UserCredential credential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (credential.user == null) return;
+
+      // Se criou, atualiza nome
+      credential.user?.updateDisplayName(name);
+      // credential.user?.updatePhotoURL(image);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'weak-passwordemail-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print('(Error) AuthFirebaseService:signup = $e');
+    }
+  }
+
+  @override
+  Future<void> logout() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  static ChatUser _toChatUser(User userFirebase) {
+   return ChatUser(
+      id: userFirebase.uid,
+      name: userFirebase.displayName ?? userFirebase.email!.split('@')[0],
+      email: userFirebase.email!,
+      imageUrl: userFirebase.photoURL ?? 'assets/images/avatar.png',
+    );
+  }
+}
